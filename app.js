fa042de946f8cabf769bc462c84f2aee7b48a0c3
LPayyapilli <luke.payyapilli@gmail.com>
2015-06-22 18:49:38 -0400

    added server

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var async = require('async');
 var express = require('express');
 var bodyParser = require('body-parser');
 var jade = require('jade');
+
+var app = express();
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('server listening at http://%s:%s', host, port);
+});
