310724fc5122cdbf3dc7d45395f5741f82a9385a
alinaw89 <ali.naw89@gmail.com>
2015-06-23 14:08:26 -0400

    adds user stories to readme, modifies app.js, modifies accounts model, and deletes pictures, statuses and users models

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # back_end
 Project 3 back-end with mongoose schemas and mongoDB
+
+
+##ABOUT
+
+
+
+##Features
+
+Landing page shows sign up area
+
+User can create a new account (Registering a user with authentication and figure out validation)
+
+User can login under a user account (figure out validation)
+
+User can see their individual account and perform CRUD actions on pictures and statuses (create crud)
+
+User can also see listing of friends/followers, statuses and pictures
+
+User can like friends/followers, statuses and pictures (optional functionality )
+
+
+##Wireframes
+
+
+
+
+##Description
+
+
+
+##Install
--- app.js
+++ app.js
@@ -12,16 +12,22 @@ var routes = require('./routes/index.js');
 app.set('view engine', 'jade');
 app.set('views', './views');
 
-var User = require('./lib/users.js');
-var Status = require('./lib/statuses.js');
-var Picture = require('./lib/pictures.js');
+
 var Listing = require('./lib/listings.js');
+var Account = require('./lib/accounts.js')
 
 mongoose.connect('mongodb://localhost/clark_back_end');
 
-app.use('/',routes);
 
-var server = app.listen(3000, function(){
+
+
+
+
+
+
+app.use('/', routes);
+
+var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('server listening at http://%s:%s', host, port);
