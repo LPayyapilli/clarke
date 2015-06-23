299c7ba3f308aff2ab5a7b6a09cafc836d9dabdc
aaronw3373 <aaronw3373@gmail.com>
2015-06-23 15:49:52 -0400

    set up rountes and login views

--- app.js
+++ app.js
@@ -2,28 +2,36 @@ var mongoose = require('mongoose');
 var async = require('async');
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var jade = require('jade');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var routes = require('./routes/index.js');
 var app = express();
 var jsonParser = bodyParser.json();
 
-var routes = require('./routes/index.js');
+var Account = require('./lib/accounts.js');
+// var Listing = require('./lib/listings.js');
 
 app.set('view engine', 'jade');
 app.set('views', './views');
 
+app.use(session({
+  secret: 'learn node',
+  resave: true,
+  saveUnititialized: false
+}));
 
-var Listing = require('./lib/listings.js');
-var Account = require('./lib/accounts.js')
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport.use(Account.createStrategy());
+// passport.serializeUser(Account.serializeUser());
+// passport.deserializeUser(Account.deserializeUser());
+
+
 
 mongoose.connect('mongodb://localhost/clark_back_end');
-
-
-
-
-
-
-
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ var Account = require('../lib/accounts.js');
 var router = express.Router();
 
 
-router.get('/', function (req, res) {
-    res.render('index', { user : req.user});
+router.get('/', function (req, res, next) {
+    res.render('index', { title: "RiddleBook", user : req.user, views req.session});
 });
 
-router.get('/register', function(req, res) {
+router.get('/register', function(req, res, next) {
     res.render('register', { });
 });
 
-router.post('/register', function(req, res) {
+router.post('/register', function(req, res, next) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
             return res.render('register', { account : account });
@@ -24,20 +24,20 @@ router.post('/register', function(req, res) {
     });
 });
 
-router.get('/login', function(req, res) {
+router.get('/login', function(req, res, next) {
     res.render('login', { user : req.user });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
+router.post('/login', passport.authenticate('local'), function(req, res, next) {
     res.redirect('/');
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function(req, res, next) {
     req.logout();
     res.redirect('/');
 });
 
-router.get('/ping', function(req, res){
+router.get('/ping', function(req, res, next){
     res.status(200).send("done!");
 });
 
