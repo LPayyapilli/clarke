6f255c15ce2f51a270337d45d6120d85e597dbe9
aaronw3373 <aaronw3373@gmail.com>
2015-06-25 09:42:19 -0400

    updated routes for delete and patch

--- public/scripts/user_actions.js
+++ public/scripts/user_actions.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#delete_user').on('click', function(event) {
     $.ajax({
       method: 'DELETE',
-      url: 'http://localhost:3000/delete/user'
+      url: 'http://localhost:3000/user/delete'
     })
       .done(function(response) {
         // document.location.href="/";
@@ -11,7 +11,7 @@ $(document).ready(function() {
   $('#patch_user').on('click', function(event) {
     $.ajax({
       method: 'PATCH',
-      url: 'http://localhost:3000/patch/user'
+      url: 'http://localhost:3000/user/patch'
     })
       .done(function(response) {
         // document.location.href="/";
