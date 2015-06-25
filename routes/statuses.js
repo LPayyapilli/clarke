de2429b8dc2810bd1441167b5988859d4128e49b
alinaw89 <ali.naw89@gmail.com>
2015-06-25 15:55:05 -0400

    makes minor changes to test

--- routes/statuses.js
+++ routes/statuses.js
@@ -30,26 +30,40 @@ router.post('/newStatus', function(req, res) {
       res.sendStatus(404);
     } else {
 
-    var newStatus = new Status();
+      var newStatus = new Status();
 
       // set the user's status
       newStatus.input = req.param('input');
       newStatus.likes = 0
       newStatus.postedAt = new Date();
-      newStatus.creator = req.user.username;
+      newStatus._creator = req.user.username;
 
       // save the status
       newStatus.save(function(err) {
-          if (err){
-              console.log('Error in Saving status: '+ err);
-              throw err;
-          }
-          console.log('Status succesful');
-          res.send(newStatus);
+        if (err) {
+          console.log('Error in Saving status: ' + err);
+          throw err;
+        }
+        console.log('Status succesful');
+        res.send(newStatus);
       });
-  }
+    }
 
   });
 });
 
+
+// /* Delete Status */
+// router.delete('/home', function(req, res){
+//   Status.remove({
+//     _id: req.params.id
+//   }, function(error){
+//     if (error){
+//       console.log(error);
+//       res.sendStatus(400);
+//     }else{
+//       res.sendStatus(204);
+//     }
+//   });
+// });
 module.exports = router;
