cc1d3faf5fff6662af21fa1b3a0d83e123a6837b
aaronw3373 <aaronw3373@gmail.com>
2015-06-28 19:10:15 -0400

    take out strange console.log

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,6 @@ module.exports = function(passport) {
 
   /* GET Profile Page */
   router.get('/home', isAuthenticated, function(req, res) {
-    console.log(res.sendStatus);
     res.render('home', {
       user: req.user
     });
