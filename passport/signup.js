9250bcd6f6f2fab63bd6e369752bb015da1d7514
aaronw3373 <aaronw3373@gmail.com>
2015-06-30 11:43:00 -0400

    add following self as default

--- passport/signup.js
+++ passport/signup.js
@@ -32,14 +32,16 @@ module.exports = function(passport){
                         newUser.email = req.param('email');
                         newUser.firstName = req.param('firstName');
                         newUser.lastName = req.param('lastName');
+                        newUser.following = [username];
+                        newUser.followedBy = [username];
 
                         // save the user
                         newUser.save(function(err) {
                             if (err){
-                                console.log('Error in Saving user: '+err);  
-                                throw err;  
+                                console.log('Error in Saving user: '+err);
+                                throw err;
                             }
-                            console.log('User Registration succesful');    
+                            console.log('User Registration succesful');
                             return done(null, newUser);
                         });
                     }
